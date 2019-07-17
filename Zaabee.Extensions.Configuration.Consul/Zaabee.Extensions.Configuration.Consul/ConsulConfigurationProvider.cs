f898891594b0a7c1e6759d529dce5b548164d923
Mutuduxf <aeondxf@live.com>
2019-07-17 16:57:52 +0800

    Develop

---
+++ Zaabee.Extensions.Configuration.Consul/Zaabee.Extensions.Configuration.Consul/ConsulConfigurationProvider.cs
@@ -0,0 +1,66 @@
+﻿using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using Consul;
+using Microsoft.Extensions.Configuration;
+
+namespace Zaabee.Extensions.Configuration.Consul
+{
+    public class ConsulConfigurationProvider : ConfigurationProvider
+    {
+        private readonly ConsulClient _consulClient;
+        private readonly string _folder;
+        private readonly string _key;
+
+        public ConsulConfigurationProvider(ConsulClient consulClient, string folder = "/", string key = null)
+        {
+            _folder = folder?.Trim();
+            _key = key?.Trim();
+            _consulClient = consulClient;
+        }
+
+        public override void Load()
+        {
+            if (!string.IsNullOrEmpty(_folder))
+            {
+                var result = _consulClient.KV.List(_folder).Result;
+                if (result == null || result.StatusCode != HttpStatusCode.OK || result.Response == null) return;
+                foreach (var item in result.Response.Where(p => p.Value != null))
+                {
+                    var json = Encoding.UTF8.GetString(item.Value);
+                    using (var memoryStream = new MemoryStream())
+                    using (var streamWriter = new StreamWriter(memoryStream))
+                    {
+                        streamWriter.Write(json);
+                        streamWriter.Flush();
+                        memoryStream.Position = 0;
+                        SetKvPair(memoryStream);
+                    }
+                }
+            }
+
+            if (!string.IsNullOrEmpty(_key))
+            {
+                var result = _consulClient.KV.Get(_key).Result;
+                if (result == null || result.StatusCode != HttpStatusCode.OK || result.Response?.Value == null) return;
+                var json = Encoding.UTF8.GetString(result.Response.Value);
+                using (var memoryStream = new MemoryStream())
+                using (var streamWriter = new StreamWriter(memoryStream))
+                {
+                    streamWriter.Write(json);
+                    streamWriter.Flush();
+                    memoryStream.Position = 0;
+                    SetKvPair(memoryStream);
+                }
+            }
+        }
+
+        private void SetKvPair(Stream stream)
+        {
+            var parser = new JsonConfigurationObjectParser();
+            foreach (var keyValuePair in parser.Parse(stream))
+                Set(keyValuePair.Key, keyValuePair.Value);
+        }
+    }
+}
---
+++ Zaabee.Extensions.Configuration.Consul/ZaabeeExtensionsConfigurationConsulTestProject/UnitTest1.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
+using Xunit;
+using Zaabee.Extensions.Configuration.Consul;
+
+namespace ZaabeeExtensionsConfigurationConsulTestProject
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            var configBuilder = new ConfigurationBuilder()
+                .AddConsul(c =>
+                {
+                    c.Address = new Uri("http://192.168.5.229:8500");
+                    c.Datacenter = "dc1";
+                    c.WaitTime = TimeSpan.FromSeconds(30);
+                },folder:"/dev");
+            var config = configBuilder.Build();
+            var hostCfg = config.GetSection("LogSystemHosts").Get<List<string>>();
+            var mongoCfgUrl = config.GetSection("LogMongo").Get<string>();
+            var mongoCfgDbName = config.GetSection("LogMongoDbName").Get<string>();
+        }
+    }
+}
